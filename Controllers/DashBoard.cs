a42625f7db9af362921191fc938973d1a2b50935
Nuraj Shaminda <76643804+Nuraj250@users.noreply.github.com>
2024-11-27 13:58:04 +0530

    dashBorard

---
+++ Controllers/DashBoard.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanelApp.Controllers
+{
+    public class DashBoard : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
